569205b0b2f0684d6f1f23f0148e2f4ad83391af
Sai Kilaru <kilarusai99@gmail.com>
2015-07-20 22:27:33 -0400

    second commit

--- js/main.js
+++ js/main.js
@@ -1,23 +1,7 @@
-document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("deviceready", onDeviceReady(), false);
 
 function onDeviceReady() {
-    document.getElementById("newCourseSubmit").onclick = function() {
-        newCourse();
-    };
+    console.log("Entering innerHTML function");
+    document.getElementById("userCourses").innerHTML = "Hello";
+    console.log("Exiting innerHTML function");
 }
-
-function newCourse() {
-    var addCourse = document.getElementById("addCourse");
-    var courseName = addCourse[0].value;
-    var period = addCourse[1].value;
-    var testAvg = addCourse[2].value;
-    var quizAvg = addCourse[3].value;
-    
-    window.localStorage.setItem(period + "CourseName", courseName);
-    window.localStorage.setItem(period + "Period", period);
-    window.localStorage.setItem(period + "TestAvg", testAvg);
-    window.localStorage.setItem(period + "QuizAvg", quizAvg);
-    
-    document.getElementById("allCourses").innerHTML = "<p>" + window.localStorage.getItem("1CourseName") + "</p>";
-}
-
