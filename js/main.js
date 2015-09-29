3765fae6cf5db0423376d309d00509d92084627e
Sai Kilaru <kilarusai99@gmail.com>
2015-09-29 00:27:36 -0400

    fixed errors

--- js/main.js
+++ js/main.js
@@ -116,15 +116,15 @@ function refreshGrades() {
 
             while (window.localStorage.getItem(j + "CourseName") === null && window.localStorage.length > 0)
                 j++;
-            if (i % 2 === 1)
-                text += "<div class=\"row\">";
+            /*if (i % 2 === 1)
+                text += "<div class=\"row\">";*/
             text += "<div class=\"col s12 m6\"><div class=\"waves-effect waves-dark width100 card course\" style=\"border-bottom: 3px solid " + borderColors[Math.floor(Math.random() * 16)] + "\">" +
                     "<span class=\"black-text card-title\"><span class=\"truncate\">" + window.localStorage.getItem(j + "CourseName") + "</span></span>" +
                     "<div class=\"grades\"><p>Test: " + window.localStorage.getItem(j + "TargetTestGrade") + "</p>" +
                     "<p>Quiz: " + window.localStorage.getItem(j + "TargetQuizGrade") + "</p></div>" +
                     "</div></div>";
-            if (i % 2 === 1)
-                text += "</div>";
+            /*if (i % 2 === 1)
+                text += "</div>";*/
             j++;
         }
         document.getElementById("userCourses").innerHTML = text;
